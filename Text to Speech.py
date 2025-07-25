ccb356ddec92565af04839e689fe0688648a3fd0
SAPCEIO <104952391+thespaceio@users.noreply.github.com>
2025-07-25 18:02:44 +0100

    Add files via upload

---
+++ Text to Speech.py
@@ -0,0 +1,11 @@
+# This program turns Text to Speech
+
+from gtts import gTTS
+import os
+
+file = open("abc.txt", 'r').read()
+
+speech = gTTS(text=file, lang='en', slow=False)
+
+speech.save("voice.mp3")
+os.system("voice.mp3")
