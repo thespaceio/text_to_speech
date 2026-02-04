cd9cddd325cab6fd671529bb7e177ae5eb9dd9c5
SAPCEIO <104952391+thespaceio@users.noreply.github.com>
2026-02-04 20:24:40 +0100

    Enhance Text to Speech program with print statements
    Added print statements to indicate reading and saving processes.

--- Text to Speech.py
+++ Text to Speech.py
@@ -1,11 +1,15 @@
 # This program turns Text to Speech
 
+
 from gtts import gTTS
 import os
 
+print("================ READING FILE =======================")
 file = open("abc.txt", 'r').read()
 
 speech = gTTS(text=file, lang='en', slow=False)
 
+print("=============== SAVING FILE ========================")
 speech.save("voice.mp3")
+
 os.system("voice.mp3")
